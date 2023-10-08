3878cb68f6c6f58bed30c6af36461c6c81df30fd
SHAHIN <shahinalambncc2014@gmail.com>
2023-10-08 23:25:26 +0600

    create basket.api and settings url

--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,7 +1,3 @@
-using Catalog.API.HostingService;
-using Catalog.API.Interfaces.Manager;
-using Catalog.API.Manager;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,20 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddHostedService<AppHostedService>();
-builder.Services.AddScoped<IProductManager, ProductManager>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
