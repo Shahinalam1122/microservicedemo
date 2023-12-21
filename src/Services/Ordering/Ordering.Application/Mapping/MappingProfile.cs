d24d54ef65b304bdfe3e37c3f72497be69395f78
SHAHIN <shahinalambncc2014@gmail.com>
2023-12-21 21:56:28 +0600

    Create Orders folder in Features folder and create Queries & Commands in Orders folder and create GetOrdersByUserName in Queries folder and create OrderVm.cs file, GetOrdersByUserQuery.cs and GetOrdersByUserHandler.cs file in GetOrdersByUserName folder and import library of MediatR and Automapper and create Mapping folder in ordering.application project and create MappingProfile.cs file in Mapping folder

---
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersByUserName;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Mapping
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        { 
+            CreateMap<Order, OrderVm>().ReverseMap();
+        }
+    }
+}
