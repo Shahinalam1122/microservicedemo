f9a45962f02842a91f34c9ffe2d4517e36f2df8f
SHAHIN <shahinalambncc2014@gmail.com>
2023-12-23 23:02:06 +0600

    create a sending email in ordering.application project while create order of a customer thats why CreateOrderCommandHandler.cs file to handle function to implement sending email obejct

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
+using Ordering.Application.Contacts.Infrastructure;
 using Ordering.Application.Contacts.Persistence;
+using Ordering.Application.Models;
 using Ordering.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -14,15 +16,29 @@ namespace Ordering.Application.Features.Orders.Commands.CreateOrder
     {
         IOrderRepository _orderRepository;
         IMapper _mapper;
-        public CreateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+        IEmailService _emailService;
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, IEmailService emailService)
         {
             _orderRepository = orderRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
         public async Task<bool> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
             var order=_mapper.Map<Order>(request);
-            return await _orderRepository.AddAsync(order);
+            //return await _orderRepository.AddAsync(order);
+
+            // sending email
+            bool isOrderPlace=await _orderRepository.AddAsync(order);
+            if (isOrderPlace)
+            {
+                Email email = new Email();
+                email.Subject = "Your Order has been placed..";
+                email.To = order.UserName;
+                email.Body = $"Dear {order.FirstName + " " + order.LastName} <br/><br/> We are excited for you to received your order ${order.Id} and with notify you one it's way. <br/> Thank for ordering from Programming Palli.";
+                await _emailService.SendEmailAsync(email);
+            }
+            return isOrderPlace;
         }
     }
 }
